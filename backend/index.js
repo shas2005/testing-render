e1fec12f25e1396538a62909a83958cdf7b08bd9
Siah Peih Wee <puaywee@gmail.com>
2023-07-06 09:32:10 +0800

    Setup Index.js for React Single Page Application #8

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const connection = require('./db'); //Import from db.js
 
-
+const path = require('path');
 
 //////////////////////////////////////////////////////
 // INIT
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/legacy", express.static("../frontend"));
-app.use("/", express.static("../frontend-react/build"));
+app.use(express.static(path.join(__dirname, '../frontend-react/build')));
 
 //////////////////////////////////////////////////////
 // DISPLAY SERVER RUNNING
@@ -44,3 +44,10 @@ app.use("/api", mainRoutes);
 app.get("/message", (req, res, next) => {
     res.send(req.query);
 });
+
+//////////////////////////////////////////////////////
+// FOR SINGLE PAGE APPLICATION
+//////////////////////////////////////////////////////
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend-react/build', 'index.html'));
+  });
