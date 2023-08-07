72beb610c9dd1da98cdf8f36cd8f8f51cc996839
shas2005 <SHASMECKA05.22@ichat.sp.edu.sg>
2023-08-07 18:33:37 +0800

    4

--- backend/db.js
+++ backend/db.js
@@ -1,37 +1,37 @@
-// //////////////////////////////////////////////////////////////////////////
-// // INCLUDES
-// //////////////////////////////////////////////////////////////////////////
-// /* Include to use .env file */
-// require('dotenv').config();
-
-// const mysql = require('mysql2')
-
-// //////////////////////////////////////////////////////////////////////////
-// // DATABASE_URL extracted from .env file
-// //////////////////////////////////////////////////////////////////////////
-// const connection = mysql.createConnection(process.env.DATABASE_URL)
-// console.log("DB Connected", connection)
-// module.exports = connection;
-
-
-
-const mysql = require('mysql2');
+//////////////////////////////////////////////////////////////////////////
+// INCLUDES
+//////////////////////////////////////////////////////////////////////////
+/* Include to use .env file */
 require('dotenv').config();
 
+const mysql = require('mysql2')
 
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-
-  }
-
-});
-
+//////////////////////////////////////////////////////////////////////////
+// DATABASE_URL extracted from .env file
+//////////////////////////////////////////////////////////////////////////
+const connection = mysql.createConnection('mysql://eio6jkyy3wvjbb82snrn:pscale_pw_vGwbs1oursxq1f4zVIbhO6b3MpwW2yETGGTQ6CKdIKK@aws.connect.psdb.cloud/ades?ssl={"rejectUnauthorized":true}')
 console.log("DB Connected", connection)
 module.exports = connection;
+
+
+
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+
+
+// const connection = mysql.createConnection({
+//   host:'aws.connect.psdb.cloud',
+//   port: 'eio6jkyy3wvjbb82snrn',
+//   user: pr,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   ssl: {
+//     rejectUnauthorized: true,
+
+//   }
+
+// });
+
+// console.log("DB Connected", connection)
+// module.exports = connection;
